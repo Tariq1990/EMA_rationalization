10856de06521ebd11d37e8f62215459a5fb11dc5
Engr.Tariq Jamal <engr.tariqjamal1990@gmail.com>
2024-11-06 14:20:54 +0500

    First Commit

---
+++ rationalization.R
@@ -0,0 +1,10 @@
+## This a file related to rationalization of teaching staff and enrolled students
+
+# load libraries
+library(tidyverse)
+
+
+# load data
+students <- read.csv("raw_data/enrollment.csv")
+teachers <- read.csv ("raw_data/Teaching Staff.csv")
+View(students)
